dc6ac40443c415c5018c74d3cdaf6733ab701d23
Bjørn Hjelle <bjornhjelle@yahoo.com>
2015-02-27 08:08:21 +0100

    removed comments...

--- provisioning/manifests/server.pp
+++ provisioning/manifests/server.pp
@@ -1,32 +1,13 @@
-# http://projects.puppetlabs.com/projects/1/wiki/File_Permission_Check_Patterns
 
-#define check_mode($mode) {
-#  exec { "/bin/chmod $mode $name":
-#    unless => "/bin/sh -c '[ $(/usr/bin/stat -c %a $name) == $mode ]'",
-#  }
-#}
 
 node default {
 
-#  class { "timezone": 
-#    region => "Europe", 
-#    locality => "Oslo",    
-#  }      
+     
     
   Exec {
     path => "/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin"
-    #require => Class["timezone"]
   }
     
-#  group { "puppet":
-#     ensure => "present"
-#  }
-#  
-#  exec { "fusion":
-#    command => "rpm -Uvh http://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-stable.noarch.rpm http://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-stable.noarch.rpm", 
-#    unless => "rpm -qa | grep rpmfusion"
-#  }
-
 
   package { ["wget", "curl", "make"]:
     ensure => present
@@ -34,7 +15,11 @@ node default {
 
   include solr_server
 
-      
+  class { "solr": 
+      user => "solr", 
+      group => "solr",
+	  home_dir => "/home/solr"    
+  }
   
 
 }
